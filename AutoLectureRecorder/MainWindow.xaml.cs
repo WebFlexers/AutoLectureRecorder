bd8556c620214a71e208d0335ddd14bc4e681d2b
mStylias <57811193+mStylias@users.noreply.github.com>
2021-02-25 19:55:14 +0200

    Added menu layout

---
+++ AutoLectureRecorder/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutoLectureRecorder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMenu_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            Brush initialBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#333856");
+            Brush pressedBrush = (SolidColorBrush)new BrushConverter().ConvertFrom("#454D73");
+            initialBrush.
+            button.Background = button.Background.Equals(initialBrush) ? pressedBrush : initialBrush;
+        }
+    }
+}
