ab20959a61ed8362d0e0960f5ff67da3d1d43614
mStylias <57811193+mStylias@users.noreply.github.com>
2021-03-13 14:08:28 +0200

    Added youtube sign in page

--- AutoLectureRecorder/MainWindow.xaml.cs
+++ AutoLectureRecorder/MainWindow.xaml.cs
@@ -1,21 +1,9 @@
 ﻿using AutoLectureRecorder.Pages;
-using AutoLectureRecorder.Selenium;
 using AutoLectureRecorder.Structure;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AutoLectureRecorder
 {
@@ -37,7 +25,7 @@ namespace AutoLectureRecorder
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            MenuRecord.IsSelected = true;  
+            MenuRecord.IsSelected = true;
         }
 
         #region Titlebar
