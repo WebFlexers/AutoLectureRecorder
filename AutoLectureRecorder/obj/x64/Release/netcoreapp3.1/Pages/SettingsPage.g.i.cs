6f009c146925ebd6c3848a28ffd098fc2a0839fb
mStylias <57811193+mStylias@users.noreply.github.com>
2021-05-26 13:55:16 +0300

    Updated chrome driver and youtube api

--- AutoLectureRecorder/obj/x64/Release/netcoreapp3.1/Pages/SettingsPage.g.i.cs
+++ AutoLectureRecorder/obj/x64/Release/netcoreapp3.1/Pages/SettingsPage.g.i.cs
@@ -79,7 +79,7 @@ namespace AutoLectureRecorder.Pages {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AutoLectureRecorder;V1.0.0.0;component/pages/settingspage.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AutoLectureRecorder;component/pages/settingspage.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Pages\SettingsPage.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
