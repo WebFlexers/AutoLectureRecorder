95c8fed2f4010e2adc514023b82935daf32382b9
StyleM <57811193+mStylias@users.noreply.github.com>
2023-04-19 19:24:51 +0300

    Fix forced .Net Framework restore
    + Target windows 10.0.19041.0 to fix the issue causing the latest version of ReactiveUI.Wpf to be restored to .Net Framework

--- tests/AutoLectureRecorder.Recorder.Tests/Services/Recording/WindowsRecorderTests.cs
+++ tests/AutoLectureRecorder.Recorder.Tests/Services/Recording/WindowsRecorderTests.cs
@@ -6,66 +6,66 @@ namespace AutoLectureRecorder.Recorder.Tests.Services.Recording;
 
 public class WindowsRecorderTests
 {
-    //[DllImport("user32.dll")]
-    //static extern IntPtr GetForegroundWindow();
+    [DllImport("user32.dll")]
+    static extern IntPtr GetForegroundWindow();
 
-    //private readonly ITestOutputHelper _output;
+    private readonly ITestOutputHelper _output;
 
-    //public WindowsRecorderTests(ITestOutputHelper output)
-    //{
-    //    _output = output;
-    //}
+    public WindowsRecorderTests(ITestOutputHelper output)
+    {
+        _output = output;
+    }
 
-    //[Fact]
-    //public async Task StartRecording_ShouldRecordScreenFor3Seconds()
-    //{
-    //    await TestSuccessfulRecording("RecordScreenTest");
-    //}
+    [Fact]
+    public async Task StartRecording_ShouldRecordScreenFor3Seconds()
+    {
+        await TestSuccessfulRecording("RecordScreenTest");
+    }
 
-    //[Fact]
-    //public async Task StartRecording_ShouldRecordWindowFor3Seconds()
-    //{
-    //    IntPtr thisWindowHandle = GetForegroundWindow();
-    //    await TestSuccessfulRecording("RecordWindowTest", thisWindowHandle);
-    //}
+    [Fact]
+    public async Task StartRecording_ShouldRecordWindowFor3Seconds()
+    {
+        IntPtr thisWindowHandle = GetForegroundWindow();
+        await TestSuccessfulRecording("RecordWindowTest", thisWindowHandle);
+    }
 
-    //private async Task TestSuccessfulRecording(string videoFileName, IntPtr? windowHandle = null)
-    //{
-    //    var logger = XUnitLogger.CreateLogger<WindowsRecorder>(_output);
-    //    var recorder = new WindowsRecorder(logger)
-    //    {
-    //        RecordingDirectoryPath = Directory.GetCurrentDirectory(),
-    //        RecordingFileName = videoFileName
-    //    };
+    private async Task TestSuccessfulRecording(string videoFileName, IntPtr? windowHandle = null)
+    {
+        var logger = XUnitLogger.CreateLogger<WindowsRecorder>(_output);
+        var recorder = new WindowsRecorder(logger)
+        {
+            RecordingDirectoryPath = Directory.GetCurrentDirectory(),
+            RecordingFileName = videoFileName
+        };
 
-    //    recorder.StartRecording(windowHandle);
+        recorder.StartRecording(windowHandle);
 
-    //    Assert.True(recorder.IsRecording);
+        Assert.True(recorder.IsRecording);
 
-    //    await Task.Delay(3000);
+        await Task.Delay(3000);
 
-    //    bool finishedSuccessfully = false;
-    //    bool recordingFailed = false;
+        bool finishedSuccessfully = false;
+        bool recordingFailed = false;
 
-    //    recorder.StopRecording()
-    //        .OnRecordingComplete(() =>
-    //        {
-    //            finishedSuccessfully = true;
-    //        })
-    //        .OnRecordingFailed(() =>
-    //        {
-    //            recordingFailed = true;
-    //        });
+        recorder.StopRecording()
+            .OnRecordingComplete(() =>
+            {
+                finishedSuccessfully = true;
+            })
+            .OnRecordingFailed(() =>
+            {
+                recordingFailed = true;
+            });
 
-    //    while (recorder.IsRecording)
-    //    {
-    //        await Task.Delay(100);
-    //    }
+        while (recorder.IsRecording)
+        {
+            await Task.Delay(100);
+        }
 
-    //    var currentDirectory = Directory.GetCurrentDirectory();
-    //    Assert.True(File.Exists(Path.Combine(currentDirectory, $"{videoFileName}.mp4")));
-    //    Assert.True(finishedSuccessfully);
-    //    Assert.False(recorder.IsRecording);
-    //    Assert.False(recordingFailed);
-    //}
+        var currentDirectory = Directory.GetCurrentDirectory();
+        Assert.True(File.Exists(Path.Combine(currentDirectory, $"{videoFileName}.mp4")));
+        Assert.True(finishedSuccessfully);
+        Assert.False(recorder.IsRecording);
+        Assert.False(recordingFailed);
+    }
 }
