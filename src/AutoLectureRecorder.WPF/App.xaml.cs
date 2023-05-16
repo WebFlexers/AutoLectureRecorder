70c7998018b23df23be8f9cb42fd92c8b95ba6f3
StyleM <57811193+mStylias@users.noreply.github.com>
2023-05-16 17:09:17 +0300

    Refactor App.xaml.cs

--- src/AutoLectureRecorder.WPF/App.xaml.cs
+++ src/AutoLectureRecorder.WPF/App.xaml.cs
@@ -1,21 +1,16 @@
 ﻿using AutoLectureRecorder.DependencyInjection;
 using AutoLectureRecorder.Sections.Login;
 using AutoLectureRecorder.Sections.MainMenu;
-using AutoLectureRecorder.Services.DataAccess;
-using AutoLectureRecorder.Services.DataAccess.Interfaces;
+using AutoLectureRecorder.Services.DataAccess.Repositories;
+using AutoLectureRecorder.Services.DataAccess.Repositories.Interfaces;
 using AutoLectureRecorder.Services.DataAccess.Seeding;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using System;
 using System.Diagnostics;
-using System.Reflection;
-using System.Security.AccessControl;
-using System.Security.Principal;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Forms;
-using AutoLectureRecorder.Resources.Themes;
 using Application = System.Windows.Application;
 
 namespace AutoLectureRecorder;
@@ -23,37 +18,21 @@ namespace AutoLectureRecorder;
 public partial class App : Application
 {
     public AppBootstrapper? Bootstrapper { get; private set; }
-
-    private readonly Mutex _mutex;
-    private readonly bool _mutexCreated;
+    private AppMutex _appMutex = new();
 
     public App()
     {
-        // Use a global mutex to stop the app from running more than once
-        string mutexId = $"Global\\{GetType().GUID}";
-
-        MutexAccessRule allowEveryoneRule = new(
-            new SecurityIdentifier(WellKnownSidType.WorldSid, null),
-            MutexRights.FullControl,
-            AccessControlType.Allow);
-        MutexSecurity securitySettings = new();
-        securitySettings.AddAccessRule(allowEveryoneRule);
-
-        _mutex = new Mutex(initiallyOwned: true, mutexId, out _mutexCreated);
-        _mutex.SetAccessControl(securitySettings);
+        _appMutex.InitializeMutex();
     }
-
-    //[DllImport("UXTheme.dll", SetLastError = true, EntryPoint = "#138")] 
-    //public static extern bool ShouldSystemUseDarkMode();
 
     protected override async void OnStartup(StartupEventArgs e)
     {
-        if (_mutexCreated == false)
-        {
-            Application.Current.Shutdown();
-        }
+        AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
+        _appMutex.HandleDuplicateAppInstance();
 
         bool showStartupWindow = true;
+
         // If we are in development populate the database with sample data
         if (Debugger.IsAttached)
         {
@@ -63,12 +42,10 @@ public partial class App : Application
                         .Seed();
         }
 
-        AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
-
         // This is required for the WebView2 to work inside the app
         Environment.SetEnvironmentVariable("WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS", "--remote-debugging-port=9222");
 
-        // Show a startup window as a loading screen while the app loads
+        // Show a startup window as a splash screen while the app loads
         StartupWindow? startupWindow = null;
         long startTime = -1;
         if (showStartupWindow)
@@ -133,6 +110,12 @@ public partial class App : Application
         
     }
 
+    // TODO: Implement this
+    private bool ShouldShowStartupWindow()
+    {
+        throw new NotImplementedException();
+    }
+
     private void CurrentDomainOnUnhandledException(object sender, UnhandledExceptionEventArgs e)
     {
         var ex = e.ExceptionObject as Exception;
@@ -150,68 +133,7 @@ public partial class App : Application
         await Log.CloseAndFlushAsync();
 
         base.OnExit(e);
-
-        // Release the mutex OnExit
-        if (_mutexCreated)
-        {
-            try
-            {
-                _mutex.ReleaseMutex();
-            }
-            catch (ApplicationException ex)
-            {
-                System.Windows.MessageBox.Show(ex.Message, ex.GetType().FullName, 
-                                               MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
-        _mutex.Dispose();
-    }
-
-    // Access to app Styles
-    public static Style? GetStyleFromResourceDictionary(string styleName, string resourceDictionaryName)
-    {
-        var resourceDictionary = new ResourceDictionary
-        {
-            Source = new Uri($"/{Assembly.GetEntryAssembly()!.GetName().Name};component/Resources/{resourceDictionaryName}",
-                UriKind.RelativeOrAbsolute)
-        };
-        return resourceDictionary[styleName] as Style;
-    }
-
-    private static ResourceDictionary? _resourceDictionary;
-    private static ColorTheme? _currentDictionaryTheme;
-
-    public static ResourceDictionary GetCurrentThemeDictionary()
-    {
-        if (_currentDictionaryTheme != null && _currentDictionaryTheme == ThemeManager.CurrentColorTheme)
-        {
-            return _resourceDictionary!;
-        }
-
-        if (_currentDictionaryTheme == null)
-        {
-            _currentDictionaryTheme = ThemeManager.CurrentColorTheme;
-        }
-
-        if (ThemeManager.CurrentColorTheme == ColorTheme.Light)
-        {
-            _resourceDictionary = new ResourceDictionary
-            {
-                Source = new Uri(
-                    $"/{Assembly.GetEntryAssembly()!.GetName().Name};component/Resources/Themes/LightTheme.xaml",
-                    UriKind.RelativeOrAbsolute)
-            };
-        }
-        else
-        {
-            _resourceDictionary = new ResourceDictionary
-            {
-                Source = new Uri(
-                    $"/{Assembly.GetEntryAssembly()!.GetName().Name};component/Resources/Themes/DarkTheme.xaml",
-                    UriKind.RelativeOrAbsolute)
-            };
-        }
-
-        return _resourceDictionary;
+        
+        _appMutex.ReleaseMutex();
     }
 }
---
+++ src/AutoLectureRecorder.WPF/AppMutex.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Threading;
+using System.Windows;
+
+namespace AutoLectureRecorder;
+
+public class AppMutex
+{
+    private Mutex _mutex;
+    private bool _mutexCreated;
+
+    public void InitializeMutex()
+    {
+        // Use a global mutex to stop the app from running more than once
+        string mutexId = $"Global\\{GetType().GUID}";
+
+        MutexAccessRule allowEveryoneRule = new(
+            new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+            MutexRights.FullControl,
+            AccessControlType.Allow);
+        MutexSecurity securitySettings = new();
+        securitySettings.AddAccessRule(allowEveryoneRule);
+
+        _mutex = new Mutex(initiallyOwned: true, mutexId, out _mutexCreated);
+        _mutex.SetAccessControl(securitySettings);
+    }
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    [System.Runtime.InteropServices.DllImport("user32.dll")]
+    [System.Security.SuppressUnmanagedCodeSecurity]
+    public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    public void HandleDuplicateAppInstance()
+    {
+        if (_mutexCreated == false)
+        {
+            // An instance is already running, bring it to the foreground
+            var currentProcess = Process.GetCurrentProcess();
+            var runningInstance = Process.GetProcessesByName(currentProcess.ProcessName)
+                .FirstOrDefault(p => p.Id != currentProcess.Id);
+
+            if (runningInstance != null)
+            {
+                ShowWindow(runningInstance.MainWindowHandle, 5);
+                SetForegroundWindow(runningInstance.MainWindowHandle);
+            }
+
+            // Shutdown the new instance
+            Application.Current.Shutdown();
+        }
+    }
+
+    public void ReleaseMutex()
+    {
+        if (_mutexCreated)
+        {
+            try
+            {
+                _mutex.ReleaseMutex();
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.Message, ex.GetType().FullName, 
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        _mutex.Dispose();
+    }
+}
