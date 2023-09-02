05256dd41c75ab4c308a42d54799c37d7f7189dd
StyleM <57811193+mStylias@users.noreply.github.com>
2023-09-02 18:36:25 +0300

    Fix theme change bugs

--- src/AutoLectureRecorder/Common/CustomControls/ThemeToggler.xaml.cs
+++ src/AutoLectureRecorder/Common/CustomControls/ThemeToggler.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System.Linq;
+using System.Reactive;
 using System.Windows;
 using System.Windows.Controls;
+using AutoLectureRecorder.Common.Messages;
 using AutoLectureRecorder.Resources.Themes.ThemesManager;
+using ReactiveUI;
 using Splat;
 
 namespace AutoLectureRecorder.Common.CustomControls;
@@ -19,10 +22,12 @@ public partial class ThemeToggler : UserControl
     private void ToggleButton_Checked(object sender, RoutedEventArgs e)
     {
         _themeManager?.SwitchToLightTheme(System.Windows.Application.Current.Windows.Cast<Window>().ToArray());
+        MessageBus.Current.SendMessage(Unit.Default, PubSubMessages.UpdateTheme);
     }
 
     private void ToggleButton_OnUnchecked(object sender, RoutedEventArgs e)
     {
         _themeManager?.SwitchToDarkTheme(System.Windows.Application.Current.Windows.Cast<Window>().ToArray());
+        MessageBus.Current.SendMessage(Unit.Default, PubSubMessages.UpdateTheme);
     }
 }
