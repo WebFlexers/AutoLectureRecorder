4ad249db6cbbb672617b16d017b1ccfd3daa3e32
StyleM <57811193+mStylias@users.noreply.github.com>
2023-09-02 18:35:34 +0300

    Create main Schedule View design #12

---
+++ src/AutoLectureRecorder/Pages/MainMenu/Schedule/ScheduleView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using ReactiveUI;
+
+namespace AutoLectureRecorder.Pages.MainMenu.Schedule;
+
+public partial class ScheduleView : ReactiveUserControl<ScheduleViewModel>
+{
+    public ScheduleView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/AutoLectureRecorder/Pages/MainMenu/Schedule/ScheduleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using AutoLectureRecorder.Common.Core;
+using AutoLectureRecorder.Common.Navigation;
+
+namespace AutoLectureRecorder.Pages.MainMenu.Schedule;
+
+public class ScheduleViewModel : RoutableViewModel
+{
+    public ScheduleViewModel(INavigationService navigationService) : base(navigationService)
+    {
+        
+    }
+}
